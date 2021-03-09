bd76cada58a5f2df39a764504b50bff337612665
Mitch Glass <52224377+mitchglass97@users.noreply.github.com>
2021-03-09 12:56:13 -0600

    Update Header.js

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,33 +1,70 @@
 // Navigation bar on top of website
 // Includes logo as well as links to Home, Leaderboard, and GitHub repo
 
-import React, {Fragment, useState} from "react";
-import {Link} from "react-router-dom";
+import React, { Fragment, useState } from "react";
+import { Link } from "react-router-dom";
+import keyboardPicture from "./keyboard.PNG";
 
 const Header = () => {
-
-    return( 
-    <Fragment>
-        <ul id="header" class="d-flex justify-content-between align-items-center">
-            <div>
-                <li id='title'>
-                    <h1>
-                        <Link className='logo-link' to="/" >
-                            type<span class='dark'>type</span>type
-                        </Link>
-                        </h1>
-                </li>
-            </div>
-            <div class="d-flex align-items-center">
-                <li><Link className='text-link' to="/" >Home</Link></li>
-                <li><Link className='text-link' to="/leaderboard">Leaderboard</Link></li>
-                <li>
-                    <a className='text-link' href="https://github.com/mitchglass97/typetypetype-web-app">GitHub Repo</a>
-                    </li>
-            </div>
-        </ul>
-    </Fragment>
-    );
-}
+	return (
+		<Fragment>
+			<ul
+				id='header'
+				class='d-flex justify-content-between align-items-center'
+			>
+				<div>
+					<li id='title'>
+						<h1>
+							<Link
+								className='logo-link'
+								to='/'
+							>
+								<img
+									src={
+										keyboardPicture
+									}
+								/>
+								type
+								<span class='dark'>
+									type
+								</span>
+								type
+							</Link>
+						</h1>
+					</li>
+				</div>
+				<div
+					class='d-flex align-items-center'
+					id='header-links'
+				>
+					<li>
+						<Link
+							className='text-link'
+							to='/'
+						>
+							Home
+						</Link>
+					</li>
+					<li>
+						<Link
+							className='text-link'
+							to='/leaderboard'
+						>
+							Leaderboard
+						</Link>
+					</li>
+					<li>
+						<a
+							className='text-link'
+							href='https://github.com/mitchglass97/typetypetype-web-app'
+						>
+							GitHub Repo
+						</a>
+					</li>
+				</div>
+			</ul>
+		</Fragment>
+	);
+};
 
 export default Header;
